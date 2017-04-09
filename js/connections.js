902546c090290bef205444e14547f3253c1dbd1e
Gennadi Heimann <genaheimann@gmail.com>
2017-04-10 00:01:50 +0200

    jaPlumb angepasst

--- js/connections.js
+++ js/connections.js
@@ -7,45 +7,39 @@
 
 $(document).ready(function() {
   jsPlumb.ready(function() {
-    jsPlumb.connect({
-      connector: ["Straight"],
-      source:"person_1",
-      target:"person_9",
-      paintStyle:{ stroke:"blue", strokeWidth:3 },
-      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
-      //overlays:[
-      //              ["Arrow" , { width:12, length:15, location:1 }]
-      //          ],
-      anchor: ["Right", "Left"],
-      endpoint:"Dot"
-    });
-    jsPlumb.connect({
-      connector: ["Straight"],
-      source:"person_1",
-      target:"person_2",
-      paintStyle:{ stroke:"blue", strokeWidth:3 },
-      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
-      anchor: ["Bottom", "Top"],
-      endpoint:"Dot"
-    });
-    jsPlumb.connect({
-      connector: ["Straight"],
-      source:"person_1",
-      target:"person_3",
-      paintStyle:{ stroke:"blue", strokeWidth:3 },
-      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
-      anchor: ["Bottom", "Top"],
-      endpoint:"Dot"
-    });
-    jsPlumb.connect({
-      connector: ["Straight"],
-      source:"person_9",
-      target:"person_3",
-      paintStyle:{ stroke:"blue", strokeWidth:3 },
-      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
-      anchor: ["Bottom", "Top"],
-      endpoint:"Dot"
-    });
+    var bezier = "Bezier";
+    var straight = "Straight";
+    var top = "Top";
+    var bottom = "Bottom";
+    
+    verheiratet("person_3_1", "person_3_2");
+    
+    // geschwister("person_3_1", top, "person_3_3", top, bezier);
+//     
+    // geschwister("person_3_1", bottom, "person_3_4", bottom, bezier);
+//       
+    // geschwister("person_4_1",top, "person_4_2", top, bezier);
+//     
+    // geschwister("person_4_1", bottom, "person_4_3", bottom, bezier);
+    
+    // geschwister("person_4_1", top, "person_4_4", top, bezier);
+//     
+    kinder("person_3_1", bottom, "person_4_1", top, straight);
+    
+    kinder("person_3_1", bottom, "person_4_2", top, straight);
+    
+    kinder("person_3_1", bottom, "person_4_3", top, straight);
+    
+    kinder("person_3_1", bottom, "person_4_4", top, straight);
+    
+    kinder("person_3_2", bottom, "person_4_1", top, straight);
+    
+    kinder("person_3_2", bottom, "person_4_2", top, straight);
+    
+    kinder("person_3_2", bottom, "person_4_3", top, straight);
+    
+    kinder("person_3_2", bottom, "person_4_4", top, straight);
+    
     jsPlumb.connect({
       connector: ["Straight"],
       source:"person_9",
@@ -73,4 +67,40 @@ $(document).ready(function() {
   // $('connection:odd').addClass('odd');
   // var connections = $('connection, inner');
   // setInterval(function() { connections.connections('update') }, 100);
+  
+  function verheiratet (node1, node2) {
+    jsPlumb.connect({
+      connector: ["Straight"],
+      source:node1,
+      target:node2,
+      paintStyle:{ stroke:"red", strokeWidth:3 },
+      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
+      anchor: ["Right", "Left"],
+      endpoint:"Dot"
+    });
+  }
+  
+  function geschwister (node1, anchorN1, node2, anchorN2, connector) {
+    jsPlumb.connect({
+      connector: ["Bezier"],
+      source:node1,
+      target:node2,
+      paintStyle:{ stroke:"blue", strokeWidth:3 },
+      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
+      anchor: [anchorN1, anchorN2],
+      endpoint:"Dot"
+    });
+  }
+  
+  function kinder (node1, anchorN1, node2, anchorN2, connector){
+    jsPlumb.connect({
+      connector: [connector],
+      source:node1,
+      target:node2,
+      paintStyle:{ stroke:"green", strokeWidth:3 },
+      endpointStyle:{ fill:"red", outlineStroke:"black", outlineWidth:1,radius:5 },
+      anchor: [anchorN1, anchorN2],
+      endpoint:"Dot"
+    });
+  }
 });
